10781c69c5d14be75e288acddd78134966250ca6
ruarai <ruarai.big@gmail.com>
2014-04-09 12:56:09 +1000

    initial commit

---
+++ SharpConfig/Config.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
+
+namespace SimpleConfig
+{
+    public class Config
+    {
+        public Dictionary<string,object> Values = new Dictionary<string, object>();
+        public string Namespace;
+        public bool AutoSave;
+
+        private string DataFile;
+        private string appData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+        public Config(string nameSpace,bool saveLocal = false,bool autoSave = false)
+        {
+            Namespace = nameSpace;
+            AutoSave = autoSave;
+
+            if (!saveLocal)
+            {
+                Directory.CreateDirectory(Path.Combine(appData, Namespace));
+
+                DataFile = Path.Combine(appData, Namespace, "config.json");
+            }
+            else
+            {
+                //find where the exe of the application is stored
+                //rather then using the working directory which may change
+                string exeDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+                //but if exeDirectory fails, we use the working directory.
+                DataFile = Path.Combine(exeDirectory ?? Environment.CurrentDirectory, nameSpace + ".json");
+            }
+
+            Load();
+        }
+
+        public dynamic this[string s]
+        {
+            get
+            {
+                return Values[s];
+            }
+            set
+            {
+                Values[s] = value;
+
+                if (AutoSave)
+                    Save();
+            }
+        }
+
+        public void Save()
+        {
+            string json = JsonConvert.SerializeObject(Values,Formatting.Indented);
+            File.WriteAllText(DataFile,json);
+        }
+
+        public void Load()
+        {
+            if (File.Exists(DataFile))
+            {
+                string json = File.ReadAllText(DataFile);
+                Values = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+            }
+        }
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Text;
+using SimpleConfig;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Config cfg = new Config("Fuck",true,true);
+
+            cfg["hello"] = "world";
+
+
+
+            cfg.Save();
+        }
+    }
+}
