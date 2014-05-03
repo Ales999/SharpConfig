cc0f66129a0ae6c92351b27d238aeffc9b2afbb9
ruarai <ruarai.big@gmail.com>
2014-05-03 22:24:30 +1000

    simple unit testing

---
+++ UnitTest/UnitTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpConfig;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void ValueTypes()
+        {
+            var standardConfig = new Config("SharpConfigTest",true);
+
+            int valueInteger = 5000;
+            standardConfig["testInteger"] = valueInteger;
+
+            DateTime valueDate = DateTime.Now;
+            standardConfig["testDate"] = valueDate;
+
+            char valueChar = 'x';
+            standardConfig["testChar"] = valueChar;
+
+            Assert.AreEqual(valueDate, standardConfig["testDate"]);
+            Assert.AreEqual(valueInteger, standardConfig["testInteger"]);
+            Assert.AreEqual(valueChar, standardConfig["testChar"]);
+        }
+
+        [TestMethod]
+        public void ReferenceTypes()
+        {
+            var standardConfig = new Config("SharpConfigTest", true);
+
+            string referenceString = "referenced string";
+            string[] referenceArray = "referenced string".Split(' ');
+
+            standardConfig["testString"] = referenceString;
+            standardConfig["testArray"] = referenceArray;
+
+            Assert.AreEqual(referenceString, standardConfig["testString"]);
+            Assert.AreEqual(referenceArray, standardConfig["testArray"]);
+        }
+
+    }
+}
